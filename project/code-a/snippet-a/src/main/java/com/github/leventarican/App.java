0b3866e6edd466508dfca4337d4cbabab0db07e1
leventarican <levent@protonmail.com>
2020-03-14 01:03:53 +0100

    maven more examples depencencies

--- project/code-a/snippet-a/src/main/java/com/github/leventarican/App.java
+++ project/code-a/snippet-a/src/main/java/com/github/leventarican/App.java
@@ -1,7 +1,18 @@
 package com.github.leventarican;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Apache Log4J 2 demo - https://logging.apache.org/log4j/2.x/manual/configuration.html.
+ * 
+ * @author Levent
+ */
 public class App {
+    private static final Logger LOGGER = LogManager.getLogger(App.class);
+    
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        LOGGER.error("Apache Log4J 2");
+        LOGGER.error("Log4J 2 is using default configuration if no configuration file is defined.");
     }
 }
---
+++ project/code-a/snippet-b/src/main/java/com/github/leventarican/App.java
@@ -0,0 +1,13 @@
+package com.github.leventarican;
+
+/**
+ * Apache Log4J 2 demo - https://logging.apache.org/log4j/2.x/manual/configuration.html.
+ * 
+ * @author Levent
+ */
+public class App {
+    
+    public static void main( String[] args ) {
+        System.out.println("apache avro");
+    }
+}
